3cfe0c9d503dd1c42d549819b31af059dfa45890
Noman Nasim <nomannasim786@yahoo.com>
2025-01-31 19:22:06 -0500

    Attribute name sanitization

--- Sources/XCStringsGen/Extensions/String+Utils.swift
+++ Sources/XCStringsGen/Extensions/String+Utils.swift
@@ -1,11 +1,28 @@
 import Foundation
 
 extension String {
-    func camelCased() -> String {
-        let components = components(separatedBy: CharacterSet(charactersIn: "_-."))
+    private var separators: String { "_-." }
+
+    private func camelCased() -> String {
+        let components = components(separatedBy: CharacterSet(charactersIn: separators))
         var camelCased = components.first ?? ""
         camelCased = camelCased.prefix(1).lowercased() + camelCased.suffix(camelCased.count - 1)
         components.dropFirst().forEach { camelCased += $0.capitalized }
         return camelCased
+    }
+
+    func attributeName() -> String? {
+        let regexSeparators = separators.replacingOccurrences(of: "-", with: "\\-")
+        var sanitized = replacingOccurrences(
+            of: "[^a-zA-Z0-9\(regexSeparators)]",
+            with: "",
+            options: .regularExpression
+        )
+
+        if sanitized.count > 0, sanitized.first?.isNumber ?? false {
+            sanitized = String(sanitized.dropFirst())
+        }
+
+        return sanitized.isEmpty ? nil : sanitized.camelCased()
     }
 }
